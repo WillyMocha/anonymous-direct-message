d80add7ddd099f0fb638467fa2516844069fe71a
Willy <willymilham28@gmail.com>
2025-03-30 15:31:41 +0700

    fix: disable interval for fetching messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,10 +62,10 @@ export default function Home() {
       }
 
       fetchMessages();
-      const interval = setInterval(fetchMessages, 30000); // Poll every 30s
+      // const interval = setInterval(fetchMessages, 30000); // Poll every 30s
 
-      return () => clearInterval(interval);
-  }, []);
+      // return () => clearInterval(interval);
+  },[]);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
